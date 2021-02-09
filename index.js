3895a488bfbe6eb43ae66c39ee83699af47175dc
chaimjfishman <chaim.fishman2@gmail.com>
2021-02-09 17:05:03 -0500

    removed cors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 var routes = require("./routes.js");
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
