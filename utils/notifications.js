df46d3218025acc0a80b5b801608db10feb76a79
chaimjfishman <chaim.fishman2@gmail.com>
2021-02-19 19:43:53 -0500

    added more debug prints

--- utils/notifications.js
+++ utils/notifications.js
@@ -28,13 +28,16 @@ async function sendNotification(tokens, title, message) {
     let chunks = expo.chunkPushNotifications(messages);
     
     let tickets = [];
-
+    
+    console.log('sendingchunks')
     // Send the chunks to the Expo push notification service. There are
     // different strategies you could use. A simple one is to send one chunk at a
     // time, which nicely spreads the load out over time:
     for (let chunk of chunks) {
         try {
+            console.log('trying to send chunk')
             let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+            console.log('ticketChunk after await');
             console.log(ticketChunk);
             tickets.push(...ticketChunk);
             // NOTE: If a ticket contains an error code in ticket.details.error, you
@@ -42,6 +45,7 @@ async function sendNotification(tokens, title, message) {
             // documentation:
             // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
         } catch (error) {
+            console.log('sendNotification error catch')
             console.error(error);
         }
     }
