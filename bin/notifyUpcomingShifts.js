0be1f2c3b9d9d20d01094482f8772e725271e9e8
chaimjfishman <chaim.fishman2@gmail.com>
2021-02-19 20:28:38 -0500

    created notifyUpcomingShifts. Removed debug prints

--- bin/notifyUpcomingShifts.js
+++ bin/notifyUpcomingShifts.js
@@ -1,9 +1,13 @@
 var notif = require('../utils/notifications.js');
+var db = require('../utils/db.js');
+
 async function notifyUpcomingShifts() {
-    const tokens = ['ExponentPushToken[0Bnu9mLhNd4t-QVeTfNMPE]'];
+    const timeRangeFromNow = 30; //TODO: set proper value
+    const shifts = await db.getUpcomingShifts(timeRangeFromNow);
+    const tokens = shifts.map(shift => shift.pushToken);
     const title = "Shift Reminder";
     const message = "Your shift begins in 10 minutes!"
     await notif.sendNotification(tokens, title, message);
-    console.log('senttttt')
-}
+    console.log('Sent complete');
+}  
 notifyUpcomingShifts();
--- utils/notifications.js
+++ utils/notifications.js
@@ -12,7 +12,6 @@ async function sendNotification(tokens, title, message) {
             console.error(`Push token ${pushToken} is not a valid Expo push token`);
             continue;
         }
-        console.log('sending notification to ' + pushToken);
         messages.push({
             to: pushToken,
             sound: 'default',
@@ -22,22 +21,18 @@ async function sendNotification(tokens, title, message) {
         })
     }
 
-    console.log('sending notification')
 
 
     let chunks = expo.chunkPushNotifications(messages);
     
     let tickets = [];
     
-    console.log('sendingchunks')
     // Send the chunks to the Expo push notification service. There are
     // different strategies you could use. A simple one is to send one chunk at a
     // time, which nicely spreads the load out over time:
     for (let chunk of chunks) {
         try {
-            console.log('trying to send chunk')
             let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-            console.log('ticketChunk after await');
             console.log(ticketChunk);
             tickets.push(...ticketChunk);
             // NOTE: If a ticket contains an error code in ticket.details.error, you
@@ -45,11 +40,9 @@ async function sendNotification(tokens, title, message) {
             // documentation:
             // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
         } catch (error) {
-            console.log('sendNotification error catch')
             console.error(error);
         }
     }
-    console.log('notification seniding competeteee')
 }
 
 
